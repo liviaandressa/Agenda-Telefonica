dbb5e1157d6711017dfa9f8e5e309f74dc80d0d0
Lívia Santos <liviaandressa61@gmail.com>
2021-12-21 16:07:08 -0300

    adicionando campos do admin à página home

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Contato
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+        
+    })
+
+def ver_contato(request, contato_id):
+    contato = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contato': contato
+        
+    })

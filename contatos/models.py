a7b25f3b582fe026726454c79e1771aac4940b11
Lívia Santos <liviaandressa61@gmail.com>
2021-12-21 16:05:34 -0300

    troca do nome de ma variável

--- contatos/models.py
+++ contatos/models.py
@@ -3,13 +3,20 @@ from django.utils import timezone
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
   
  
 class Contato(models.Model):
     nome = models.CharField(max_length=100)
     sobrenome = models.CharField(max_length=100, blank=True) 
-    teleone = models.CharField(max_length=15)
+    telefone = models.CharField(max_length=15)
     email = models.CharField(max_length=100, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao =  models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
